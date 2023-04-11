a6845726b1d3d62fc2f53c56d94842fbc3964cb4
Dave Tyson <davetysonbass@gmail.com>
2023-04-11 16:20:18 -0400

    line to add an s to http to make it https for book imgs

--- src/components/Form.js
+++ src/components/Form.js
@@ -193,14 +193,13 @@ const Form = () => {
                             newBookVotes = 0;
                         }
 
-                        //handle the book image in cse the book has no image
+                        //handle the book image in case the book has no image
                         let bookImg = "";
                         if(bookDataObj[index].volumeInfo.imageLinks) {
-                            // console.log("true");
                             bookImg = bookDataObj[index].volumeInfo.imageLinks.thumbnail;
+                            bookImg = bookImg.slice(0, 4) + "s" + bookImg.slice(4);
                         }
                         else {
-                            // console.log("false");
                             bookImg = null;
                         }
 
